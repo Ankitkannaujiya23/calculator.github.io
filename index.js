4ac3311522199aaec4c6dc4a450dc4fd2b083a69
Ankit Kannaujiya <ankitkannaujiya666@gmail.com>
2022-02-22 23:36:36 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,32 @@
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons){
+    item.addEventListener('click' , (e)=>{
+        buttonText=e.target.innerText;
+        console.log("the button text is :", buttonText);
+
+     
+            
+
+        if(buttonText=='X'){
+            buttonText='*';
+            screenValue+=buttonText;
+            screen.value=screenValue;
+        }
+        else if(buttonText=='C'){
+            screenValue='';
+            screen.value=screenValue;
+        }
+        else if(buttonText=='='){
+            screen.value=eval(screenValue);  //eval is a JS function which  evaluate the value 
+            screenValue='';
+        }
+        else{
+            screenValue+=buttonText;
+            screen.value=screenValue;
+            
+        }
+       
+    })
+}
